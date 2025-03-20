9c2351732bb1cc760393e741294128876dcbc694
MuhammadArthur21 <yusyafarthurmuhammad@gmail.com>
2025-03-20 13:07:10 +0700

    Add files via upload

---
+++ PolylinesController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\PolylinesModel;
+
+class PolylinesController extends Controller
+{
+    public function __construct()
+    {
+        $this->polylines = new PolylinesModel();
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // validate data
+        $request->validate([
+            'name' => 'required|unique:polylines,name',
+            'description' => 'required',
+            'geom_polyline' => 'required',
+        ],
+        [
+            'name.required' => 'Name is required',
+            'name.unique' => 'Name already exists',
+            'description.required' => 'Description is required',
+            'geom_polyline.required' => 'Geometry is required',
+        ]);
+
+        $data = [
+            'geom' => $request->geom_polyline,
+            'name' => $request->name,
+            'description' => $request->description
+        ];
+
+        // create data
+        if (!$this->polylines->create($data)) {
+            return redirect()->route('map')->with('error', 'Polyline failed to add!');
+        }
+
+        return redirect()->route('map')->with('success', 'Polyline has been added!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
